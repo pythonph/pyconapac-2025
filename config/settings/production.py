4c649c5a1e37e423ef8cb5e873bc7cbc32b2f241
Zorex <zorexsalvo@gmail.com>
2024-10-31 19:24:01 +0800

    Add pycon-apac to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,8 +19,18 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 DEBUG = env("DEBUG")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["ph.pycon.org", "pycon-2024.python.ph", "pycon.python.ph", "localhost"]
-CSRF_TRUSTED_ORIGINS = ['https://pycon-2024.python.ph', 'https://pycon.python.ph']
+ALLOWED_HOSTS = [
+    "ph.pycon.org",
+    "pycon-2024.python.ph",
+    "pycon.python.ph",
+    "localhost",
+    "pycon-apac.python.ph",
+]
+CSRF_TRUSTED_ORIGINS = [
+    "https://pycon-2024.python.ph",
+    "https://pycon.python.ph",
+    "https://pycon-apac.python.ph"
+]
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
